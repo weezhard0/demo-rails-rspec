5534b4a5da9aeb644d53518e01727b2e211a10a6
WeezHard <hi@weezhard.dev>
2023-01-26 10:57:01 +0000

    Move before block inside context

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper do
   describe '#show_mood' do
-    before do
-      allow(helper).to receive(:happy?).and_return(true)
-    end
-
     context 'when is happy' do
+      before do
+        allow(helper).to receive(:happy?).and_return(true)
+      end
+
       it { expect(helper.show_mood).to eq('Happy') }
     end
 
